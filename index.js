d2150d4e7b1e3e5fa10776c661ca725b74ec0690
kmelter <kmelter97@gmail.com>
2022-04-16 15:53:51 -0500

    add multiple employees

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ const employeeCardStore = ``;
     //call fs.writeFile() after the for loop
     //ensure classes are present in HTML template and in cards (bootstrap)
 
+
+const generateTemplate = async () => {
+    for (let i = 0; i < employeeStore.length; i++) {
+        //TODO: write template
+    }
+}
+
 const managerInformationPrompt = async () => {
     const prompt = [
         {
@@ -37,7 +44,30 @@ const managerInformationPrompt = async () => {
         }
     ]
     const answers = await inquirer.prompt(prompt);
-    //console.log("answers in manager prompt function", answers);
+    return answers;
+}
+
+const engineerInformationPrompt = async () => {
+    const prompt = [
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter github username:'
+        }
+    ]
+    const answers = await inquirer.prompt(prompt);
+    return answers;
+}
+
+const internInformationPrompt = async () => {
+    const prompt = [
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter name school or university:'
+        }
+    ]
+    const answers = await inquirer.prompt(prompt);
     return answers;
 }
     
@@ -65,25 +95,73 @@ const employeeInformationPrompt = async () => {
             choices: ['Intern', 'Engineer', 'Manager']
         }
     ]
+
     const answers = await inquirer.prompt(prompt);
-    //console.log("anwswers in employee prompt function", answers);
+    
     if (answers.userType === 'Manager') {
         let managerInput = await managerInformationPrompt();
         let newManager = new Manager(answers, managerInput);
         employeeStore.push(newManager);
         console.log(employeeStore);
+        
+        const prompt = [
+            {
+                type: 'confirm',
+                name: 'addEmployee',
+                message: 'Would you like to add a new employee?'
+            }
+        ]
+        const answer = await inquirer.prompt(prompt);
+        if (answer.addEmployee === true) {
+            employeeInformationPrompt();
+        } else {
+            //use employeeStore to make template
+            console.log('finish');
+        }
+    }
+    else if (answers.userType === 'Intern') {
+        let internInput = await internInformationPrompt();
+        let newIntern = new Intern(answers, internInput);
+        employeeStore.push(newIntern);
+        console.log(employeeStore);
+
+        const prompt = [
+            {
+                type: 'confirm',
+                name: 'addEmployee',
+                message: 'Would you like to add a new employee?'
+            }
+        ]
+        const answer = await inquirer.prompt(prompt);
+        if (answer.addEmployee === true) {
+            employeeInformationPrompt();
+        } else {
+            //use employeeStore to make template
+            console.log('finish');
+        }
+    }
+    else if (answers.userType === 'Engineer') {
+        let engineerInput = await engineerInformationPrompt();
+        let newEngineer = new Engineer(answers, engineerInput);
+        employeeStore.push(newEngineer);
+        console.log(employeeStore);
+
+        const prompt = [
+            {
+                type: 'confirm',
+                name: 'addEmployee',
+                message: 'Would you like to add a new employee?'
+            }
+        ]
+        const answer = await inquirer.prompt(prompt);
+        if (answer.addEmployee === true) {
+            employeeInformationPrompt();
+        } else {
+            //use employeeStore to make template
+            console.log('finish');
+        }
     }
     return answers;
-}
-
-
-
-const engineerInformationPrompt = async () => {
-
-}
-
-const internInformationPrompt = async () => {
-
 }
 
 const addEmployeePrompt = async () => {
