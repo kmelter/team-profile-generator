37f474570ea5d09729848cc51173e397869a28c9
kmelter <kmelter97@gmail.com>
2022-04-16 13:48:43 -0500

    manager functionality

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
+const { ChildProcess } = require('child_process');
 const employeeStore = [];
 const employeeCardStore = ``;
 
@@ -27,47 +28,77 @@ const employeeCardStore = ``;
     //call fs.writeFile() after the for loop
     //ensure classes are present in HTML template and in cards (bootstrap)
 
-const employeeInformationPrompt = async () => {
-    const prompt = [
-        {
-            type: 'input',
-            name: 'name',
-            message: 'Enter your name:'
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'Enter your employee ID:'
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter your email:'
-        }
-    ]
-    const answers = await inquirer.prompt(prompt);
-    //console.log("anwswers in employee prompt function", answers);
-    return answers;
-}
-
 const managerInformationPrompt = async () => {
     const prompt = [
         {
             type: 'input',
             name: 'officeNumber',
-            message: 'Enter your office number:'
+            message: 'Enter office number:'
         }
     ]
     const answers = await inquirer.prompt(prompt);
     //console.log("answers in manager prompt function", answers);
     return answers;
 }
+    
+const employeeInformationPrompt = async () => {
+    const prompt = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter employee name:'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee ID:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter employee email:'
+        },
+        {
+            type: 'list',
+            name: 'userType',
+            message: 'Choose which to add:',
+            choices: ['Intern', 'Engineer', 'Manager']
+        }
+    ]
+    const answers = await inquirer.prompt(prompt);
+    //console.log("anwswers in employee prompt function", answers);
+    if (answers.userType === 'Manager') {
+        let managerInput = await managerInformationPrompt();
+        let newManager = new Manager(answers, managerInput);
+        employeeStore.push(newManager);
+        console.log(employeeStore);
+    }
+    return answers;
+}
+
+
+
+const engineerInformationPrompt = async () => {
+
+}
+
+const internInformationPrompt = async () => {
+
+}
+
+const addEmployeePrompt = async () => {
+
+}
 
 const initApp = async() => {
-    let generalInformation = await employeeInformationPrompt();
-    let managerInput = await managerInformationPrompt();
-    new Manager(generalInformation, managerInput);
-    //console.log('answers returned', generalInformation, managerInput);
+    employeeInformationPrompt();
+    
+    // let generalInformation = await employeeInformationPrompt();
+    // let managerInput = await managerInformationPrompt();
+    // let firstManager = new Manager(generalInformation, managerInput);
+    // employeeStore.push(firstManager);
+    // console.log(employeeStore);
+    
 }
 
 initApp();
